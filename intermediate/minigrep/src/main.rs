b237b6932de643b5319f0bd78c9ef981fddc2c11
hiro <yassirelyakine@gmail.com>
2022-09-20 19:54:28 +0100

    more rust

--- intermediate/minigrep/src/main.rs
+++ intermediate/minigrep/src/main.rs
@@ -2,16 +2,14 @@ use std::{env, process};
 use minigrep::Config;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    let config = Config::build(&args).unwrap_or_else(|err| {
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("\nProblem parsing arguments: {err}"); //print to stderr instead of stdout
         process::exit(1);
     });
 
 
     if let Err(e) = minigrep::run(config) {
-        eprintln!("Application error: {e}");
+        eprintln!("\nApplication error: {e}");
         process::exit(1);
     }
 }
